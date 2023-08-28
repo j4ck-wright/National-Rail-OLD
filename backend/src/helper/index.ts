0eaa591c2ce9bb0f4c5447ce9e2848b5de0f3189
Jack Wright <jackw221001@gmail.com>
2023-08-28 21:45:14 +0100

    requestBuilder added

--- backend/src/helper/index.ts
+++ backend/src/helper/index.ts
@@ -1,3 +1,3 @@
 export { XMLtoJSON } from "./formatConverter";
 export { sanitiseJSON } from "./sanitiser";
-export { requestBuilder } from "./requestBuilder";
+export { buildXMLString } from "./requestBuilder";
--- backend/src/helper/requestBuilder.ts
+++ backend/src/helper/requestBuilder.ts
@@ -1,18 +1,17 @@
-type options = {
-  crs?: string;
-  numRows?: string | number;
-};
+import { serviceBoardOptions } from "../templates/serviceBoardParams";
+import { serviceDetailsOptions } from "../templates/getServiceDetails";
 
-const defaultOptions: options = {
-  numRows: 2,
-};
-
-export function requestBuilder(
+export function buildXMLString(
   template: string,
   token: string,
-  options: options
-) {
-  throw Error("Not Implemented Yet");
-  //TODO
-  //Check params in xml template e.g. %%NUMROWS%% and replace with value in options or default val if not in options
+  options: serviceBoardOptions | serviceDetailsOptions
+): string {
+  let output = template.replace("!!TokenValue!!", token);
+  for (const [key, value] of Object.entries(options)) {
+    output = output.replace("!!" + `${key}` + "!!", `${value}`);
+  }
+  const regex = new RegExp("!!(.*?)!!", "g");
+  output = output.replaceAll(regex, "");
+
+  return output;
 }
